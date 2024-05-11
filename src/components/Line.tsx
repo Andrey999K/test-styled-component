521f3e112716821b9ad073cc64efafb8e1aba65b
Andrey999K <andrey1234xxxx@mail.ru>
2024-05-11 23:27:16 +0300

    refactor: migration styled component StyledLine on tailwind

--- src/components/Line.tsx
+++ src/components/Line.tsx
@@ -1,16 +1,11 @@
-import styled from "styled-components";
 import React, { ReactNode } from "react";
-
-const StyledLine = styled.div<LineProps>`
-  font-size: 24px;
-  color: ${props => props.color || props.theme.colors.primary};
-`;
 
 type LineProps = {
   children?: ReactNode | ReactNode[];
   color?: string;
 };
 
-export const Line: React.FC<LineProps> = props => {
-  return <StyledLine {...props} />;
+export const Line: React.FC<LineProps> = ({ color, children }) => {
+  const colorClass = `text-${color || "primary"}`;
+  return <div className={`text-2xl leading-9 ${colorClass}`}>{children}</div>;
 };
